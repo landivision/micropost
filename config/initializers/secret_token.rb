a89b7e6b1156d1b07a582d9b4bd16c961585398e
landivision <landivision@yahoo.com>
2012-02-08 16:16:02 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropost::Application.config.secret_token = 'd5fcb687841f50100ce494dccfb05e8094465a4a748457b59a727e329ebd6b02a58dbf237cc0e0b0037e8f985e99196faef9063466db01a45b4db53043583dd4'
